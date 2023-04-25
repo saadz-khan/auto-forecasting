5c8a8c3cb0ec438b08861e2a798ff8a988529c46
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-04-26 01:42:59 +0500

    Fixed preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -3,9 +3,9 @@ import pandas as pd
 import datetime as dt
 
 gen_1=pd.read_csv('./generation_data.csv')
-gen_1.drop('PLANT_ID',1,inplace=True)
+gen_1.drop(['PLANT_ID'],axis=1,inplace=True)
 sens_1= pd.read_csv('./weather_data.csv')
-sens_1.drop('PLANT_ID',1,inplace=True)
+sens_1.drop(['PLANT_ID'],axis=1,inplace=True)
 
 #format datetime
 gen_1['DATE_TIME']= pd.to_datetime(gen_1['DATE_TIME'],format='%d-%m-%Y %H:%M')

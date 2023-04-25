98ab36c7596799eac49d7b404b7b66b3bb727681
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-04-26 02:01:05 +0500

    Final fixes

--- preprocessing.py
+++ preprocessing.py
@@ -3,9 +3,9 @@ import pandas as pd
 import datetime as dt
 
 gen_1 = pd.read_csv('./generation_data.csv')
-gen_1.drop(columns=['PLANT_ID'], axis=1, inplace=True)
+gen_1.drop(columns=['PLANT_ID', 'SOURCE_KEY'], axis=1, inplace=True)
 sens_1 = pd.read_csv('./weather_data.csv')
-sens_1.drop(columns=['PLANT_ID'], axis=1, inplace=True)
+sens_1.drop(columns=['PLANT_ID', 'SOURCE_KEY'], axis=1, inplace=True)
 
 # Format datetime
 gen_1['DATE_TIME'] = pd.to_datetime(gen_1['DATE_TIME'])
@@ -15,5 +15,4 @@ df_gen = gen_1.groupby('DATE_TIME').sum().reset_index()
 df_gen['time'] = df_gen['DATE_TIME'].dt.time
 
 final_df = pd.merge(df_gen, sens_1, how='outer', on='DATE_TIME')
-final_df.drop(['SOURCE_KEY'], axis=1, inplace=True)
 final_df.to_csv('./final_data.csv')

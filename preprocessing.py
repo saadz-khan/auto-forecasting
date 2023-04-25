81a1c6e6ea87906b9d24dbee9a7f989b91e012e3
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-04-26 01:23:35 +0500

    Final fix

--- preprocessing.py
+++ preprocessing.py
@@ -2,18 +2,18 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
-gen_1 = pd.read_csv('./generation_data.csv')
-gen_1.drop(columns=['PLANT_ID', 'SOURCE_KEY'], axis=1, inplace=True)
-sens_1 = pd.read_csv('./weather_data.csv')
-sens_1.drop(columns=['PLANT_ID', 'SOURCE_KEY'], axis=1, inplace=True)
+gen_1=pd.read_csv('/content/Plant_1_Generation_Data.csv')
+gen_1.drop('PLANT_ID',1,inplace=True)
+sens_1= pd.read_csv('/content/Plant_1_Weather_Sensor_Data.csv')
+sens_1.drop('PLANT_ID',1,inplace=True)
 
-# Format datetime
-gen_1['DATE_TIME'] = pd.to_datetime(gen_1['DATE_TIME'], dayfirst=True)
-sens_1['DATE_TIME'] = pd.to_datetime(sens_1['DATE_TIME'], dayfirst=True)
+#format datetime
+gen_1['DATE_TIME']= pd.to_datetime(gen_1['DATE_TIME'],format='%d-%m-%Y %H:%M')
+sens_1['DATE_TIME']= pd.to_datetime(sens_1['DATE_TIME'],format='%Y-%m-%d %H:%M:%S')
 
-df_gen = gen_1.groupby('DATE_TIME').sum().reset_index()
-df_gen['time'] = df_gen['DATE_TIME'].dt.time
+df_gen=gen_1.groupby('DATE_TIME').sum().reset_index()
+df_gen['time']=df_gen['DATE_TIME'].dt.time
 
 final_df = pd.merge(df_gen, sens_1, how='outer', on='DATE_TIME')
-#final_df.drop(['SOURCE_KEY'], axis=1, inplace=True)
+final_df.drop(['SOURCE_KEY'], axis=1, inplace=True)
 final_df.to_csv('./final_data.csv')

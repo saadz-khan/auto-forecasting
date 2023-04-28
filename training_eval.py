7e794e4f55ac573bc29fa71985d7bae019f7ac6b
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-04-28 19:32:20 +0500

    Added future forecasting in training_eval.py

--- training_eval.py
+++ training_eval.py
@@ -50,7 +50,7 @@ def build_and_train_lstm_model(train_X, train_y, test_X, test_y, window_size, le
     
     return model, history
 
-def plot_predicted_vs_actual(test_X, test_y, test_pred, df, train_size, window_size):
+def plot_predicted_vs_actual(test_X, test_y, test_pred, df, train_size, window_size, scaler, future_df=None):
     test_pred = scaler.inverse_transform(np.concatenate((test_X[:, -1, :-1], test_pred), axis=1))[:, -1]
     test_y = scaler.inverse_transform(np.concatenate((test_X[:, -1, :-1], test_y.reshape(-1, 1)), axis=1))[:, -1]
 
@@ -60,6 +60,8 @@ def plot_predicted_vs_actual(test_X, test_y, test_pred, df, train_size, window_s
     plt.figure(figsize=(15, 5))
     plt.plot(test_df.index, test_df['Predicted'], label='Predicted')
     plt.plot(test_df.index, test_df['Actual'], label='Actual')
+    if future_df is not None:
+        plt.plot(future_df.index, future_df['Predicted'], label='Future Predicted')
     plt.title('Predicted vs Actual Daily Yield')
     plt.xlabel('Date')
     plt.ylabel('Daily Yield')
@@ -67,24 +69,65 @@ def plot_predicted_vs_actual(test_X, test_y, test_pred, df, train_size, window_s
     plt.show()
 
 
-# Preprocess data
-df = pd.read_csv('final_data.csv')
-df = preprocess_data(df)
+def train_and_evaluate():
+    # Preprocess data
+    df = pd.read_csv('final_data.csv')
+    df = preprocess_data(df)
 
-# Split and normalize data
-train_data, test_data, scaler = split_and_normalize_data(df)
+    # Split and normalize data
+    train_data, test_data, scaler = split_and_normalize_data(df)
 
-# Create windowed dataset
-window_size = 24
-train_X, train_y = create_dataset(train_data, window_size)
-test_X, test_y = create_dataset(test_data, window_size)
+    # Create windowed dataset
+    window_size = 24
+    train_X, train_y = create_dataset(train_data, window_size)
+    test_X, test_y = create_dataset(test_data, window_size)
 
-# Build and train LSTM model
-model, history = build_and_train_lstm_model(train_X, train_y, test_X, test_y, window_size)
+    # Build and train LSTM model
+    model, history = build_and_train_lstm_model(train_X, train_y, test_X, test_y, window_size)
 
-# Make predictions on test data
-test_pred = model.predict(test_X)
+    # Make predictions on test data
+    test_pred = model.predict(test_X)
 
-# Plot predicted vs actual values
-train_size = int(len(df) * 0.8)
-plot_predicted_vs_actual(test_X, test_y, test_pred, df, train_size, window_size)
+    # Create test_df DataFrame
+    train_size = int(len(df) * 0.8)
+    test_pred_transformed = scaler.inverse_transform(np.concatenate((test_X[:, -1, :-1], test_pred), axis=1))[:, -1]
+    test_y_transformed = scaler.inverse_transform(np.concatenate((test_X[:, -1, :-1], test_y.reshape(-1, 1)), axis=1))[:, -1]
+    test_df = pd.DataFrame({'DATE_TIME': df.index[train_size + window_size:], 'Predicted': test_pred_transformed, 'Actual': test_y_transformed})
+    test_df.set_index('DATE_TIME', inplace=True)
+
+    # Get the last 24 hours of the test data
+    last_24_hours = df[-96:]
+
+    # Normalize the last 24 hours of the test data
+    last_24_hours = scaler.transform(last_24_hours)
+
+    # Create a windowed dataset with a window size of 24
+    future_X, _ = create_dataset(last_24_hours, window_size)
+
+    # Make predictions on the future dataset
+    future_pred = model.predict(future_X)
+
+    # Inverse transform the predictions to get the original values
+    future_pred = scaler.inverse_transform(np.concatenate((future_X[:, -1, :-1], future_pred), axis=1))[:, -1]
+
+    # Create a datetime range for the next day with the same frequency as the data
+    date_range = pd.date_range(start=df.index[-1], periods=96, freq='15min')
+
+    # Create a new date range with a smaller number of periods
+    future_range = pd.date_range(start=date_range[-1], periods=len(future_pred)+1, freq='15min')[1:]
+
+    # Create a dataframe with the predicted values and the datetime range
+    future_df = pd.DataFrame({'DATE_TIME': future_range, 'Predicted': future_pred})
+
+    # Set DATE_TIME as index
+    future_df.set_index('DATE_TIME', inplace=True)
+
+    # Concatenate the two dataframes
+    future_df_fin = pd.concat([test_df.tail(1), future_df], axis=0)
+
+    # Plot the results
+    plot_predicted_vs_actual(test_X, test_y, test_pred, df, train_size, window_size, scaler, future_df=future_df_fin)
+
+    return test_df, test_pred, future_df_fin
+
+test_df, test_pred, future_df = train_and_evaluate()

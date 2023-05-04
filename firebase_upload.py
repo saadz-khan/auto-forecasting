c6b8829117dde5c3a740dcd4bc8f43c240777ee6
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-05-04 15:47:55 +0500

    Fixed minor errors 2

--- firebase_upload.py
+++ firebase_upload.py
@@ -4,6 +4,13 @@ from firebase_admin import credentials
 from firebase_admin import db
 import json
 
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, float):
+            return str(obj)
+        return super(CustomJSONEncoder, self).default(obj)
+
 aDict = {
   "type": "service_account",
   "project_id": "forecasting-1609",
@@ -35,8 +42,14 @@ df = pd.read_csv('./future_predictions.csv')
 # Convert the dataframe to a dictionary
 data_dict = df.to_dict(orient='records')
 
+# Serialize the data_dict using the custom JSON encoder
+json_data = json.dumps(data_dict, cls=CustomJSONEncoder)
+
+# Deserialize the JSON data back to a Python object
+data_dict_str = json.loads(json_data)
+
 # Upload the data to Firebase under a new child node named 'dataframe'
 data_ref = db.reference('dataframe')
-data_ref.set(data_dict)
+data_ref.set(data_dict_str)
 
 print('Data uploaded successfully')
